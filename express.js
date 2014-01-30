ad068d20f8d389ab19be764e0b4c66def81c30bf
Daniel Matthew <hello@danmatthew.co.uk>
2014-01-30 13:36:07 +0000

    2.15
    Adding a basic API

--- express.js
+++ express.js
@@ -3,9 +3,25 @@
 var express = require('express');
 var app = express.createServer();
 
+app.listen(8000);
+
+var tweets = [];
+
 app.get('/', function(req, res){
 	// No need to define headers ourselves!
 	res.send('Welcome to NodeTwitter');
 });
 
-app.listen(8000);
+app.post('/send', express.bodyParser(), function(req, res){
+	if(req.body && req.body.tweet) {
+		tweets.push(req.body.tweet);
+		res.send({status: 'ok', message: 'Tweet received'});
+	} else {
+		// No tweet
+		res.send({status: 'nok', message: 'No tweet received'});
+	}
+});
+
+app.get('/tweets', function(req, res){
+	res.send(tweets);
+});
