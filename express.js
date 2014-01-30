c2523fcb4eb1b6f006f1c28fde71e98c7281ef14
Daniel Matthew <hello@danmatthew.co.uk>
2014-01-30 13:46:41 +0000

    2.16
    Test for POST api

--- express.js
+++ express.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express.createServer();
 
-app.listen(8000);
+app.listen(8000); // Asynchronous
 
 var tweets = [];
 
@@ -12,7 +12,7 @@ app.get('/', function(req, res){
 	res.send('Welcome to NodeTwitter');
 });
 
-app.post('/send', express.bodyParser(), function(req, res){
+app.post('/send', express.bodyParser(), function(req, res){ // bodyParser = middleware
 	if(req.body && req.body.tweet) {
 		tweets.push(req.body.tweet);
 		res.send({status: 'ok', message: 'Tweet received'});
